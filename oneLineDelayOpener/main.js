d794c0fe9ebd712118d6291323a068e41c16f857
iHok <ihokya@gmail.com>
2023-12-14 12:02:16 +0900

    javascriptの仮コミット

---
+++ oneLineDelayOpener/main.js
@@ -0,0 +1,11 @@
+
+//一行ごと読み込み配列をリターン
+readTextToOneLine=(e)=>{
+
+}
+
+//最初の関数
+const main=()=>{
+    let arrayURL=readTextToOneLine(document.querySelector("textarea"));//URL配列を宣言
+    //配列から一行ごと読み込み、行数ｘ指定時間でsettimeoutでopenする
+}
